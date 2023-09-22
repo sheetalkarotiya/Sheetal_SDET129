e79128ca011db81652e4eab7cd0d3bd61a4babe4
sheet <sheet@LAPTOP-68FQM03A.mynetworksettings.com>
2023-09-22 18:25:58 -0400

    Ds.portal page

--- src/test/java/stepdefinations/DSPortal.java
+++ src/test/java/stepdefinations/DSPortal.java
@@ -7,43 +7,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.messages.types.Duration;
 
 public class DSPortal {
-
 	
 	public WebDriver driver;
 	 String URL = "https://dsportalapp.herokuapp.com/";
 	
 	 By getstarted = By.xpath("//button[text()='Get Started']");
+
+	
+
+	@Given("user open the application of DSPortalapp <https:\\/\\/dsportalapp.herokuapp.com\\/>")
+	public void user_open_the_application_of_ds_portalapp_https_dsportalapp_herokuapp_com() {
+	  
+		
+	System.getProperty("webdriver.chrome.driver", "C:\\Users\\sheet\\eclipse-workspace sdet129\\Selenium_Project_1\\src\\test\\resources\\drivers");
+	 driver = new ChromeDriver();
+	 driver.get(URL);
 	 
-	
 
-@Given("user open the application of DSPortalapp <https:\\/\\/dsportalapp.herokuapp.com\\/>")
-public void user_open_the_application_of_ds_portalapp_https_dsportalapp_herokuapp_com() {
-   
-   System.getProperty("C:\\Users\\sheet\\eclipse-workspace\\Selenium_Project_01\\src\\test\\resources\\drivers");
-   driver = new ChromeDriver();
-   driver.get(URL);
+	}
 
-  
-   
-	
-}
+	@Then("user land on DS Portal Application page")
+	public void user_land_on_ds_portal_application_page() {
+	  
+	   
+	}
 
-@When("user click to get to get started button")
-public void user_click_to_get_to_get_started_button() {
-   
-	driver.findElement(getstarted).click();
-    
-   
-}
+	@When("user click to get to get started button")
+	public void user_click_to_get_to_get_started_button() {
+	  
+		driver.findElement(getstarted).click();
 
-@Then("user navigate to homepage")
-public void user_navigate_to_homepage() {
-   
-   
-}
+	   
+	}
 
+	@Then("user navigate to homepage")
+	public void user_navigate_to_homepage() {
+	  
+	   
+	}	
 
 }
--- src/test/resources/features/DSPortal.feature
+++ src/test/resources/features/DSPortal.feature
@@ -1,17 +1,34 @@
 @DSPortal
-Feature: DSPortal
+Feature: DSPortal Application
 I want to use this template for my feature file
 
+
+
+Background:
+Given  user open the application of DSPortalapp <https://dsportalapp.herokuapp.com/> 
+Then   user land on DS Portal Application page
+
+
+
+@testcase001
+Scenario Outline: Open the  of DSPortalapp Application
+Given  user open the application of DSPortalapp <https://dsportalapp.herokuapp.com/> 
+Then   user land on DS Portal Application page
+
+Examples: 
+| url                                | 
+| https://dsportalapp.herokuapp.com/ |
 
 
 
 
 @testcase002
-Scenario Outline: Open the homepage of DSPortalapp
-Given user open the application of DSPortalapp <https://dsportalapp.herokuapp.com/> 
+Scenario Outline: Open the  of DSPortalapp Application
 When user click to get to <button> button
 Then user navigate to homepage
 Examples: 
-| url | button |
+| url                                     | button |
 | https://dsportalapp.herokuapp.com/ | get started |
+
+
 
